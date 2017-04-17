a31af5346f4e17372a994acf0ec413e49b65198f
gazon1 <newbox2517@inbox.ru>
2017-04-17 12:32:36 +0300

    Delete Input.h

--- Input.h
+++
@@ -1,7 +0,0 @@
-#include <SFML/Graphics.hpp>
-
-class Input {
-public:
-	void catch_events(sf::Window& m_window); //В этой ф-ии находится цикл который вызывает handle_event
-	void handle_event(sf::Window& m_window, Event m_event); //эта ф-ия обрабатывает события
-};
